1c7feee3c34402c7dd4125dea26307a69c5e8521
alessandroNessi <alessandronessi@msn.com>
2021-10-01 16:28:32 +0200

    added header

---
+++ src/components/Albums.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Album />    
+  </div>
+</template>
+
+<script>
+// import axios from "axios",
+import Album from "../components/Album.vue";
+export default {
+  name: "Albums",
+  components: {
+    Album,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/common.scss";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="headerContainer">
+    <img src="../assets/img/spotify-logo.png" :style="`height:${headerHeight}`" alt="logo">
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  props: ["headerHeight"],
+};
+</script>
+<style lang="scss" scoped>
+@import "../assets/style/common.scss";
+  .headerContainer{
+    padding: 10px;
+    background-color: $bg_header_color;
+    img{
+      height: 100%;
+    }
+  }
+</style>
